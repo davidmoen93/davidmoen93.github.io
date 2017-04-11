431dffec6520bfa51a5811b821dd7d8b8482fb66
davidmoen93 <dmoen93@gmail.com>
2017-04-11 23:54:14 +0100

    2
    2

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<title>David Moen Portfolio</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-		<link rel="stylesheet" type="text/css" href="http://davidmoen93.github.io/style.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
 		<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
