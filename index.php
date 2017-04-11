c96dcdcd838532d0b14c5f360ed6439a6a5c3f3d
davidmoen93 <dmoen93@gmail.com>
2017-04-11 23:55:35 +0100

    1
    1

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h1>Hello World</h1>
-<p>I'm hosted with GitHub Pages.</p>
-</body>
-</html>
+<?php include 'header.php'; ?>
+
+	<div class="home-header cf">
+
+		<h1>Hi, I'm David. I design and develop websites.</p>
+
+		<a href="#me">Learn more about me</a>
+
+	</div>
+	<div id="me" class="home-container cf">
+
+
+		<ul class="cf">
+			<li>Themec Grotto</li>
+			<li>K-Scaff</li>
+			<li>Brockagh</li>
+			<li>Quadrant</li>
+			<li>Phlexcom</li>
+			<li>B4B</li>
+			<li>LED</li>
+			<li>MyFitness</li>
+		</ul>
+	</div>
+
+<?php include 'footer.php'; ?>
